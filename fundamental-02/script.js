4ae5ba740f32c38ec33707af1f5d5cbbdc453543
Kaustubh Rai <98734149+kaustubh0955@users.noreply.github.com>
2022-12-11 19:03:35 +0530

    Update script.js

--- fundamental-02/script.js
+++ fundamental-02/script.js
@@ -98,3 +98,29 @@ console.log(calcAverage(3,4,5));
 const scoreDolphins=calcAverage(44,23,71);
 const scoreKoalas=calcAverage(65,54,49);
 console.log(scoreDolphins,scoreKoalas);
+
+//Coding Challenge 1
+
+const calcAverage=(a,b,c)=>(a-+b+c)/3;
+console.log(calcAverage(3,4,5));
+
+const scoreDolphins=calcAverage(44,23,71);
+const scoreKoalas=calcAverage(65,54,49);
+
+console.log(scoreDolphins,scoreKoalas);
+const checkWinner=function(avgDolphins,avgKoalas)
+{
+    if(avgDolphins>=2*avgKoalas)
+    {
+        console.log('Dolphins win '+ avgDolphins);
+    }
+    else if(avgKoalas >=2*avgDolphins)
+    {
+        console.log('Koalas win'+avgKoalas);
+    }
+    else{
+        console.log('No team wins');
+    }
+}
+checkWinner(scoreDolphins,scoreKoalas);
+checkWinner(576,111);
